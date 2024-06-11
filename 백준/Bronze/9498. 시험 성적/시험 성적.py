21eee94d87d429e2adf87cf003e804a3cdb49f04
yh-2670 <yuminjulieha@gmail.com>
2024-06-11 17:00:42 +0900

    [Bronze V] Title: 시험 성적, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,12 @@
+a=int(input())
+
+if 90<=a and a<=100:
+    print('A')
+elif 80<=a and a<=89:
+    print('B')
+elif 70<=a and a<=79:
+    print('C')
+elif 60<=a and a<=69:
+    print('D')
+else:
+    print('F')
